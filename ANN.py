bd24f2d950d97fe3c79e017801eeff5f380bb478
Toshan Kirit Jagani <tkjagani@gmail.com>
2024-06-05 23:19:05 +0530

    Changing things up a bit

--- ANN.py
+++ ANN.py
@@ -11,7 +11,7 @@ training_data = datasets.FashionMNIST(
     transform=ToTensor()
 )
 
-testing_data = datasets.FashionMNIST(
+testing_data = datasets.MNIST(
     root = 'Data',
     train = False,
     download = True,
@@ -37,66 +37,25 @@ class Net(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
-        self.net = nn.Sequential(
-            nn.Linear(784, 16),
-            nn.ReLU(),
-            nn.Linear(16, 16),
-            nn.ReLU(),
-            nn.Linear(16, 10),
-            nn.Softmax()
-        )
+        self.l1 = nn.Linear(784, 512)
+        self.r1 = nn.ReLU()
+        self.l2 = nn.Linear(512, 512)
+        self.r2 = nn.ReLU()
+        self.l3 = nn.Linear(512, 10)
+        self.s1 = nn.Softmax()
     
     def forward(self, x):
-        x = self.flatten(x)
-        probabilities = self.net(x)
-        return(probabilities)
+        out = self.flatten(x)
+        out = self.l1(out)
+        out = self.r1(out)
+        out = self.l2(out)
+        out = self.r2(out)
+        out = self.l3(out)
+        out = self.s1(out)
+        return(out)
 
 model = Net().to(device)
 print(model)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-
-def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X,y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
-
-        pred = model(X)
-        loss = loss_fn(pred, y)
-
-        loss.backward()
-        optimizer.zero_grad()
-        optimizer.step()
-
-        if(batch % 100 == 0):
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f'Loss : {loss:>7f} [{current:>5d}/{size:>5d}]')
-
-
-def test(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
-
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch {t+1}\n==============================")
-    train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
-print("Model Trained!")
-
-torch.save(model.state_dict(), "Saved_Model/model.pth")
-print("Model saved : )")
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
