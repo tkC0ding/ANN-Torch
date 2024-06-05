2d1bf373e19868be1f3fb72d46b5fc0fb96e179c
Toshan Kirit Jagani <tkjagani@gmail.com>
2024-06-05 22:08:47 +0530

    downloading datasets
    Not executing, execute after comleting the code

--- ANN.py
+++ ANN.py
@@ -3,3 +3,17 @@ from torch import nn
 from torch.utils.data import dataloader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+
+training_data = datasets.FashionMNIST(
+    root = 'Data',
+    train = True,
+    download=True,
+    transform=ToTensor()
+)
+
+testing_data = datasets.FashionMNIST(
+    root = 'Data',
+    train = False,
+    download = True,
+    transform=ToTensor()
+)
